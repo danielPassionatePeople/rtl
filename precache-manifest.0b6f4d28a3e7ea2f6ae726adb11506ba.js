9aff6537f6f03a8cf82f5d18f004aa79e257646f
Daniel Alarcón Amador <danielalarconamador@Daniels-MacBook-Pro.local>
2018-10-05 18:15:36 +0200

    Updates

---
+++ precache-manifest.0b6f4d28a3e7ea2f6ae726adb11506ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c11a509931acef33159",
+    "url": "/rtl/static/js/5.0c11a509.chunk.js"
+  },
+  {
+    "revision": "292516ffca8895376c1c",
+    "url": "/rtl/static/js/0.292516ff.chunk.js"
+  },
+  {
+    "revision": "b585cd0433870ad31181",
+    "url": "/rtl/static/js/1.b585cd04.chunk.js"
+  },
+  {
+    "revision": "6f15701c13f6f4ebe22c",
+    "url": "/rtl/static/js/main.6f15701c.chunk.js"
+  },
+  {
+    "revision": "3b93bde88b5c2f45dccd",
+    "url": "/rtl/static/js/3.3b93bde8.chunk.js"
+  },
+  {
+    "revision": "c5e41071d5952a760744",
+    "url": "/rtl/static/js/4.c5e41071.chunk.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/rtl/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/rtl/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "d094fb9496540df020f2",
+    "url": "/rtl/static/js/runtime~main.d094fb94.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/rtl/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/rtl/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "0c11a509931acef33159",
+    "url": "/rtl/static/css/5.5ca7c239.chunk.css"
+  },
+  {
+    "revision": "b585cd0433870ad31181",
+    "url": "/rtl/static/css/1.b706d31e.chunk.css"
+  },
+  {
+    "revision": "6c0993da0ff32feaff26e9f65f9798e4",
+    "url": "/rtl/index.html"
+  }
+];
